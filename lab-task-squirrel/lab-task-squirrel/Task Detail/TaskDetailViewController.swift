5360da6af42302823aa49c2bf023c6ce8d3f0840
noaboa07 <143129234+noaboa07@users.noreply.github.com>
2024-09-03 19:36:51 -0600

    Step 7 Complete
    Can now effectively add photos to pinpoints and complete tasks

--- lab-task-squirrel/lab-task-squirrel/Task Detail/TaskDetailViewController.swift
+++ lab-task-squirrel/lab-task-squirrel/Task Detail/TaskDetailViewController.swift
@@ -25,9 +25,11 @@ class TaskDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // TODO: Register custom annotation view
+        // Register custom annotation view
+        mapView.register(TaskAnnotationView.self, forAnnotationViewWithReuseIdentifier: TaskAnnotationView.identifier)
 
-        // TODO: Set mapView delegate
+        // Set mapView delegate
+        mapView.delegate = self
 
         // UI Candy
         mapView.layer.cornerRadius = 12
@@ -174,17 +176,17 @@ extension TaskDetailViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         // Dismiss the picker
         picker.dismiss(animated: true)
-
+        
         // Get the selected image asset (we can grab the 1st item in the array since we only allowed a selection limit of 1)
         let result = results.first
-
+        
         // Get image location
         // PHAsset contains metadata about an image or video (ex. location, size, etc.)
         guard let assetId = result?.assetIdentifier,
               let location = PHAsset.fetchAssets(withLocalIdentifiers: [assetId], options: nil).firstObject?.location else {
             return
         }
-
+        
         print("📍 Image location coordinate: \(location.coordinate)")
         
         
@@ -192,35 +194,52 @@ extension TaskDetailViewController: PHPickerViewControllerDelegate {
         guard let provider = result?.itemProvider,
               // Make sure the provider can load a UIImage
               provider.canLoadObject(ofClass: UIImage.self) else { return }
-
+        
         // Load a UIImage from the provider
         provider.loadObject(ofClass: UIImage.self) { [weak self] object, error in
-
+            
             // Handle any errors
             if let error = error {
-              DispatchQueue.main.async { [weak self] in self?.showAlert(for:error) }
-            
+                DispatchQueue.main.async { [weak self] in self?.showAlert(for:error) }
+                
             }
-
+            
             // Make sure we can cast the returned object to a UIImage
             guard let image = object as? UIImage else { return }
-
+            
             print("🌉 We have an image!")
-
+            
             // UI updates should be done on main thread, hence the use of `DispatchQueue.main.async`
             DispatchQueue.main.async { [weak self] in
-
+                
                 // Set the picked image and location on the task
                 self?.task.set(image, with: location)
-
+                
                 // Update the UI since we've updated the task
                 self?.updateUI()
-
+                
                 // Update the map view since we now have an image an location
                 self?.updateMapView()
             }
         }
     }
+}
+    extension TaskDetailViewController: MKMapViewDelegate {
+        
+        // Implement mapView(_:viewFor:) delegate method.
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+            // Dequeue the annotation view for the specified reuse identifier and annotation.
+            // Cast the dequeued annotation view to your specific custom annotation view class, `TaskAnnotationView`
+            // 💡 This is very similar to how we get and prepare cells for use in table views.
+            guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: TaskAnnotationView.identifier, for: annotation) as? TaskAnnotationView else {
+                fatalError("Unable to dequeue TaskAnnotationView")
+            }
+
+            // Configure the annotation view, passing in the task's image.
+            annotationView.configure(with: task.image)
+            return annotationView
+        }
+    }
     
 
-}
